cb670e5856102620f67fd9a470130731939a889b
Bart Hageman <barthageman1@gmail.com>
2024-04-01 14:12:48 +0200

    Change colorscheme, lualine > mini

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -1,16 +1,17 @@
-return {
-  {
-    'nvim-lualine/lualine.nvim',
-    event = 'VimEnter',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    opts = {
-      options = {
-        theme = 'tokyonight',
-        section_separators = { left = '', right = '' },
-        -- section_separators = { left = '', right = ''},
-        -- component_separators = { left = '·', right = '·'}
-        component_separators = { left = '', right = '' },
-      },
-    },
-  },
-}
+return {}
+-- return {
+--   {
+--     'nvim-lualine/lualine.nvim',
+--     event = 'VimEnter',
+--     dependencies = { 'nvim-tree/nvim-web-devicons' },
+--     opts = {
+--       options = {
+--         theme = 'gruvbox-material',
+--         section_separators = { left = '', right = '' },
+--         -- section_separators = { left = '', right = ''},
+--         -- component_separators = { left = '·', right = '·'}
+--         component_separators = { left = '', right = '' },
+--       },
+--     },
+--   },
+-- }
