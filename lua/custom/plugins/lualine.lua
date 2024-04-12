22790b6a00bb3fe550a71eee8649329a06e13114
Bart Hageman <barthageman1@gmail.com>
2024-04-12 16:27:42 +0200

    Back to lualine

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -1,17 +1,20 @@
-return {}
--- return {
---   {
---     'nvim-lualine/lualine.nvim',
---     event = 'VimEnter',
---     dependencies = { 'nvim-tree/nvim-web-devicons' },
---     opts = {
---       options = {
---         theme = 'gruvbox-material',
---         section_separators = { left = '', right = '' },
---         -- section_separators = { left = '', right = ''},
---         -- component_separators = { left = '·', right = '·'}
---         component_separators = { left = '', right = '' },
---       },
---     },
---   },
--- }
+--return {}
+return {
+  {
+    'nvim-lualine/lualine.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+      options = {
+        theme = 'rose-pine',
+        section_separators = { left = '', right = '' },
+        -- section_separators = { left = '', right = ''},
+        -- component_separators = { left = '·', right = '·'}
+        component_separators = { left = '', right = '' },
+      },
+      sections = {
+        lualine_c = { 'harpoon2' },
+      },
+    },
+  },
+}
