a08fd081f6f7291ac20985ee7bc3a3f94f792e70
BartHageman <barthageman1@gmail.com>
2024-04-14 01:01:08 +0200

    Switch back to lazygit

--- lua/custom/plugins/fterm.lua
+++ lua/custom/plugins/fterm.lua
@@ -1,25 +1,26 @@
-return {
-  {
-    'numToStr/FTerm.nvim',
-    opts= {
-      border = 'rounded',
-    },
-    config = function()
-      local fterm = require("FTerm")
-      local lazygit = fterm:new({
-        ft = 'lazygit', -- You can also override the default filetype, if you want
-        cmd = "lazygit",
-        dimensions = {
-          height = 0.9,
-          width = 0.9
-        }
-      })
-
-      -- Use this to toggle gitui in a floating terminal
-      vim.keymap.set('n', '<leader>g', function()
-        lazygit:toggle()
-      end)
-    end
-
-  }
-}
+return {}
+-- return {
+--   {
+--     'numToStr/FTerm.nvim',
+--     opts= {
+--       border = 'rounded',
+--     },
+--     config = function()
+--       local fterm = require("FTerm")
+--       local lazygit = fterm:new({
+--         ft = 'lazygit', -- You can also override the default filetype, if you want
+--         cmd = "lazygit",
+--         dimensions = {
+--           height = 0.9,
+--           width = 0.9
+--         }
+--       })
+--
+--       -- Use this to toggle gitui in a floating terminal
+--       vim.keymap.set('n', '<leader>g', function()
+--         lazygit:toggle()
+--       end)
+--     end
+--
+--   }
+-- }
