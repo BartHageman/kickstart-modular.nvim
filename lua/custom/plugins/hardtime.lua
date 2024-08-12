4310ecb1cc69b68455920522d559dcb80cc80c01
Bart Hageman <barthageman1@gmail.com>
2024-08-12 19:40:21 +0200

    toggle hardtime

--- lua/custom/plugins/hardtime.lua
+++ lua/custom/plugins/hardtime.lua
@@ -1,7 +1,10 @@
 return {
-  {
-    'm4xshen/hardtime.nvim',
-    dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
-    opts = {},
-  },
+  -- {
+  --   'm4xshen/hardtime.nvim',
+  --   dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
+  --   opts = {
+  --     disabled_filetypes = { 'harpoon', 'qf', 'netrw', 'NvimTree', 'lazy', 'mason', 'oil' },
+  --     disable_mouse = false,
+  --   },
+  -- },
 }
