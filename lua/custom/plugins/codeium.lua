f78174a44ead137ed27f0302d1b3a7cc051f4a4b
Bart Hageman <barthageman1@gmail.com>
2024-08-12 19:40:04 +0200

    remove codium

--- lua/custom/plugins/codeium.lua
+++ lua/custom/plugins/codeium.lua
@@ -1,15 +1,4 @@
-return {
-  "Exafunction/codeium.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "hrsh7th/nvim-cmp",
-  },
-  config = function()
-    require("codeium").setup({})
-  end,
-}
-
-
+return {}
 -- return {
 --   "Exafunction/codeium.vim",
 --   --"Exafunction/codeium.nvim",
