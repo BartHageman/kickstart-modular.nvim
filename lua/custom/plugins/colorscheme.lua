5fce628e38edb07ad83a16012eedfcd4f8456c2a
Bart Hageman <barthageman1@gmail.com>
2024-08-13 17:22:08 +0200

    Actually fix gitsigns

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -15,21 +15,22 @@ return {
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
-      vim.api.nvim_set_hl(0, 'GitSignsAdd', { link = 'GitSignsAdd' })
-      vim.api.nvim_set_hl(0, 'GitSignsAddLn', { link = 'GitSignsAddLn' })
-      vim.api.nvim_set_hl(0, 'GitSignsAddNr', { link = 'GitSignsAddNr' })
-      vim.api.nvim_set_hl(0, 'GitSignsChange', { link = 'GitSignsChange' })
-      vim.api.nvim_set_hl(0, 'GitSignsChangeLn', { link = 'GitSignsChangeLn' })
-      vim.api.nvim_set_hl(0, 'GitSignsChangeNr', { link = 'GitSignsChangeNr' })
-      vim.api.nvim_set_hl(0, 'GitSignsChangedelete', { link = 'GitSignsChange' })
-      vim.api.nvim_set_hl(0, 'GitSignsChangedeleteLn', { link = 'GitSignsChangeLn' })
-      vim.api.nvim_set_hl(0, 'GitSignsChangedeleteNr', { link = 'GitSignsChangeNr' })
-      vim.api.nvim_set_hl(0, 'GitSignsDelete', { link = 'GitSignsDelete' })
-      vim.api.nvim_set_hl(0, 'GitSignsDeleteLn', { link = 'GitSignsDeleteLn' })
-      vim.api.nvim_set_hl(0, 'GitSignsDeleteNr', { link = 'GitSignsDeleteNr' })
-      vim.api.nvim_set_hl(0, 'GitSignsTopdelete', { link = 'GitSignsDelete' })
-      vim.api.nvim_set_hl(0, 'GitSignsTopdeleteLn', { link = 'GitSignsDeleteLn' })
-      vim.api.nvim_set_hl(0, 'GitSignsTopdeleteNr', { link = 'GitSignsDeleteNr' })
+
+      vim.api.nvim_set_hl(0, 'GitSignsAdd', { link = 'Added' })
+      vim.api.nvim_set_hl(0, 'GitSignsAddLn', { link = 'Added' })
+      vim.api.nvim_set_hl(0, 'GitSignsAddNr', { link = 'Added' })
+      vim.api.nvim_set_hl(0, 'GitSignsChange', { link = 'Changed' })
+      vim.api.nvim_set_hl(0, 'GitSignsChangeLn', { link = 'Changed' })
+      vim.api.nvim_set_hl(0, 'GitSignsChangeNr', { link = 'Changed' })
+      vim.api.nvim_set_hl(0, 'GitSignsChangedelete', { link = 'Removed' })
+      vim.api.nvim_set_hl(0, 'GitSignsChangedeleteLn', { link = 'Removed' })
+      vim.api.nvim_set_hl(0, 'GitSignsChangedeleteNr', { link = 'Removed' })
+      vim.api.nvim_set_hl(0, 'GitSignsDelete', { link = 'Removed' })
+      vim.api.nvim_set_hl(0, 'GitSignsDeleteLn', { link = 'Removed' })
+      vim.api.nvim_set_hl(0, 'GitSignsDeleteNr', { link = 'Removed' })
+      vim.api.nvim_set_hl(0, 'GitSignsTopdelete', { link = 'Removed' })
+      vim.api.nvim_set_hl(0, 'GitSignsTopdeleteLn', { link = 'Removed' })
+      vim.api.nvim_set_hl(0, 'GitSignsTopdeleteNr', { link = 'Removed' })
     end,
   },
 }
